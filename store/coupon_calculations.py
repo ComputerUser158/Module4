e4204e27704684c3b79ea4d6d7e04dec562ada1d
ComputerUser158 <oneringraw@gmail.com>
2020-09-21 15:18:50 -0500

    First commit fail

---
+++ store/coupon_calculations.py
@@ -0,0 +1,25 @@
+"""
+Author: Rawley Collins
+program calculates the shipping cost for the user
+"""
+
+
+def calculate_price(price, cash_coupon, percent_coupon):
+    with_cashcoupon = price - cash_coupon
+    with_percentcoupon = with_cashcoupon - (with_cashcoupon * percent_coupon/100)
+    subtotal_tax = with_percentcoupon * 1.06
+    shipping = 0
+    if with_percentcoupon < 10:
+        shipping = 5.95
+    elif with_percentcoupon < 30:
+        shipping = 7.95
+    elif with_percentcoupon < 50:
+        shipping = 11.95
+    else:
+        shipping = 0
+    return subtotal_tax + shipping
+
+
+if __name__ == '__main__':
+    x = calculate_price(5.23, 10, 15)
+    print(x)
---
+++ test/test_coupon_calculations.py
@@ -0,0 +1,24 @@
+import unittest
+from store import coupon_calculations as coupon
+
+
+class MyTestCase(unittest.TestCase):
+    def test_price_under_ten(self):
+        self.assertAlmostEqual(5.84, coupon.calculate_price(5.99, 5, 15), 2)
+        self.assertAlmostEqual(5.64, coupon.calculate_price(4.68, 5, 10), 2)
+        self.assertAlmostEqual(8.49, coupon.calculate_price(7.99, 5, 20), 2)
+        self.assertAlmostEqual(1.65, coupon.calculate_price(5.23, 10, 15), 2)
+        self.assertAlmostEqual(0.19, coupon.calculate_price(3.96, 10, 10), 2)
+        self.assertAlmostEqual(2.75, coupon.calculate_price(6.23, 10, 20), 2)
+
+    """def test_price_under_between_ten_thirty(self):
+        self.assertAlmostEqual(6.84, coupon.calculate_price(5.99, 5, 15), 2)
+        self.assertAlmostEqual(5.64, coupon.calculate_price(4.68, 5, 10), 2)
+        self.assertAlmostEqual(8.49, coupon.calculate_price(7.99, 5, 20), 2)
+        self.assertAlmostEqual(1.65, coupon.calculate_price(5.23, 10, 15), 2)
+        self.assertAlmostEqual(0.19, coupon.calculate_price(3.96, 10, 10), 2)
+        self.assertAlmostEqual(2.75, coupon.calculate_price(6.23, 10, 20), 2)
+"""
+
+if __name__ == '__main__':
+    unittest.main()
